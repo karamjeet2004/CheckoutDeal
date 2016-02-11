433027bf8b970618d0479bf6f82386253820dccc
karamjeet2004 <karamjeet2004@yahoo.co.in>
2016-02-11 19:55:05 +0530

    Pushing changes to git

---
+++ CheckoutDealDAO/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutDealDAO.Entities
+{
+    [Table("user")]
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
---
+++ CheckoutDealWS/DealService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CheckoutDealWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DealService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DealService.svc or DealService.svc.cs at the Solution Explorer and start debugging.
+    public class DealService : IDealService
+    {
+        public string DoWork()
+        {
+            return "Hello World";
+        }
+    }
+}
